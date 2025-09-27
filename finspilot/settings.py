37ff195d93874fb998e654b515816bd017708867
PrivateAbushaqra <shaqraabd@gmail.com>
2025-09-27 23:49:50 +0300

    إصلاح أخطاء في القوالب والإعدادات وتحديث الترجمات

--- finspilot/settings.py
+++ finspilot/settings.py
@@ -88,6 +88,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.i18n',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'core.context_processors.currency_context',
