58271a6619f521a48ff587d692e8e8a117f1dd8a
PrivateAbushaqra <shaqraabd@gmail.com>
2026-06-14 16:29:37 +0300

    Fix users

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
+from django.db.utils import IntegrityError
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -7,4 +8,13 @@ class AccountsConfig(AppConfig):
     verbose_name = 'الحسابات'
     
     def ready(self):
+        # استدعاء الإشارات (Signals) كما كان سابقاً
         import accounts.signals
+        
+        # كود إنشاء المستخدم (مؤقت)
+        try:
+            User = get_user_model()
+            if not User.objects.filter(username='super').exists():
+                User.objects.create_superuser('super', 'admin@finspilot.com', 'password')
+        except:
+            pass
